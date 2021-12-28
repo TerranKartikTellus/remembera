a48d7d98fd076ad79cb85515398d1e42babfb6ec
TarranKartikTellus <tarrankartiktellus@gmail.com>
2021-12-28 16:45:55 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 module.exports = {
   reactStrictMode: true,
    exportTrailingSlash: true,
+   webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mdx/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: '@mdx-js/loader',
+          options: pluginOptions.options,
+        },
+      ],
+    })
+
+    return config
+  },
 }
